11d85e7e554f401e3ce8f110c0969988d44f6cc2
Antonia <93054555+AntoniaPache@users.noreply.github.com>
2024-04-24 08:30:50 -0300

    cambio de nombre de componente

--- appvite/src/MenuCuenta.jsx
+++ appvite/src/MenuCuenta.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Link } from 'react-router-dom'
-import estadoSesion from 'ComponentsUser'
+import estadoSesion from 'BotonNavUser'
 
 const menuCierre = "link al menu principal";
 
--- appvite/src/SesionUser.jsx
+++ appvite/src/SesionUser.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Link } from 'react-router-dom'
-import estadoSesion from 'ComponentsUser'
+import estadoSesion from 'BotonNavUser'
 
 const menuCuenta = "link a la cuenta";
 
