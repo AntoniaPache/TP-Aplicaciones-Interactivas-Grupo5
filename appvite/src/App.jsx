14fdf4529c50fb9cb6590300d76321334bb8c8ca
Antonia <93054555+AntoniaPache@users.noreply.github.com>
2024-04-28 15:02:23 -0300

    Detalle Producto añadido

--- appvite/src/App.jsx
+++ appvite/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
 import 'react-router-dom';
-import SesionUser from './SesionUser.jsx'
+import DetalleProducto from './DetalleProducto';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -30,8 +30,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <div>
-      </div>
     </>
   )
 }
---
+++ appvite/src/DetalleProducto.jsx
@@ -0,0 +1,38 @@
+import {useState} from 'react';
+
+function DetalleProducto() {
+
+    const [cant, setCant] = useState(0)
+
+    return(
+        <div className="detalle-container">
+            <div className="imagen">
+                <img src="" alt="" />
+            </div>
+            <div className="datos">
+                <h1 id="encabezado">Nombre del Producto</h1>
+                <span id="precio">$XX.XX</span>
+                <form id="formularioCompra">
+                    <div className="tallas">
+                        <button>S</button>
+                        <button>M</button>
+                        <button>L</button>
+                        <button>XL</button>
+                    </div>
+                    <div className="cantidades">
+                        <div className="cant">{cant}</div>
+                        <div className="controlCant">
+                            <button onClick={() => setCant((count) => count + 1)}>+</button>
+                            <button onClick={() => setCant((count) => count - 1)}>-</button>
+                        </div>
+                    </div>
+                    <div>
+                        <button id="Compra">Comprar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default DetalleProducto;
